63d3272c694654a0c96798a970a18f0a89259cad
Tristan <tristan.santiago@ymail.com>
2019-10-28 19:32:23 -0400

    Added login url and redirect url to 'settings.py'.

--- backend/Capstone/Capstone/settings.py
+++ backend/Capstone/Capstone/settings.py
@@ -128,6 +128,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = "accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
