b9d6c9bc2da43ba19608d9ea51e0905237c3f545
Tristan <tristan.santiago@ymail.com>
2019-10-28 18:30:50 -0400

    Registered Custom User Model in 'admin.py'.

--- backend/Capstone/users/admin.py
+++ backend/Capstone/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # add_form =
+    # form =
+    model = CustomUser
+    list_display = ["username", "email", "is_staff"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
