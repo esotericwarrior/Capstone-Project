f9e766e5c172109c1283d471d850246999693c2e
Tristan <tristan.santiago@ymail.com>
2019-10-28 20:22:54 -0400

    Defined Post Class Model in '/posts/models.py'.

--- backend/Capstone/posts/models.py
+++ backend/Capstone/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings    # Import Custom User Class from settings.py
 
-# Create your models here.
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name="posts")
+
+    def __str__(self):
+        return self.content
