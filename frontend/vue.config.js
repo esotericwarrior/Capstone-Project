0c02fabc4a9977cf0c736b4bca5f111d92dc8b4a
Tristan <tristan.santiago@ymail.com>
2019-10-29 19:50:02 -0400

    Fixed typo in 'vue.config.js'.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
     // On Linux you might want to set publicPath: "http://0.0.0.0:8080/"
     publicPath: "http://127.0.0.1:8080/",
     outputDir: './dist/',
-    transpileDependencies: ["vuetify"]
-    
+    transpileDependencies: ["vuetify"],
+
     chainWebpack: config => {
 
         config
