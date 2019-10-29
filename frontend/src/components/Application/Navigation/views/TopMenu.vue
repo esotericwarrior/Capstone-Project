0447c3ed9afbbb531dfb2cb4c356e93bf43e8170
Tristan <tristan.santiago@ymail.com>
2019-10-29 19:25:03 -0400

    Added Home button icon.

--- frontend/src/components/Application/Navigation/views/TopMenu.vue
+++ frontend/src/components/Application/Navigation/views/TopMenu.vue
@@ -2,6 +2,9 @@
   <div>
     <v-toolbar color="">
       <v-spacer></v-spacer>
+      <v-btn class="ml-4" icon to="/">
+        <v-icon large>mdi-home</v-icon>
+      </v-btn>
     </v-toolbar>
   </div>
 </template>
