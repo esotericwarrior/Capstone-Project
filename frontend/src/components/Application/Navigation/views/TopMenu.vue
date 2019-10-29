df7878d4f11228909275a168996815c9baa6e3f9
Tristan <tristan.santiago@ymail.com>
2019-10-29 19:25:40 -0400

    Added 'v-divider' next to Home button.

--- frontend/src/components/Application/Navigation/views/TopMenu.vue
+++ frontend/src/components/Application/Navigation/views/TopMenu.vue
@@ -3,8 +3,9 @@
     <v-toolbar color="">
       <v-spacer></v-spacer>
       <v-btn class="ml-4" icon to="/">
-        <v-icon large>mdi-home</v-icon>
+        <v-icon>mdi-home</v-icon>
       </v-btn>
+      <v-divider class="mx-4" inset vertical></v-divider>
     </v-toolbar>
   </div>
 </template>
