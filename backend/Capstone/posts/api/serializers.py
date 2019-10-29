658aa8ea8fce7d19db69c3a52363cac2e34b768b
Tristan <tristan.santiago@ymail.com>
2019-10-28 20:48:28 -0400

    Created Post Serializer Class.

--- backend/Capstone/posts/api/serializers.py
+++ backend/Capstone/posts/api/serializers.py
@@ -25,3 +25,25 @@ class CommentSerializer(serializers.ModelSerializer):
 
     def get_post_slug(self, instance):
         return instance.post.slug
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    created_at = serializers.SerializerMethodField()
+    slug = serializers.SlugField(read_only=True)
+    comments_count = serializers.SerializerMethodField()
+    user_has_commented = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        exclude = ["updated_at"]
+
+    def get_created_at(self, instance):
+        return instance.created_at.strftime("%B %d, %Y")    # Name of month, date with a digit, year (e.g. December 25, 2019)
+
+    def get_comments_count(self, instance):
+        return instance.comments.count()
+
+    def get_user_has_commented(self, instance):
+        request = self.context.get("request")
+        return instance.comments.filter(author=request.user).exists()
