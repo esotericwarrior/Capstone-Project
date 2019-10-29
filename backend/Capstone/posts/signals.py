a24e96d0cdb26cd219e638b0a521203fac0449d4
Tristan <tristan.santiago@ymail.com>
2019-10-28 20:31:51 -0400

    Created 'signals.py' to create slug based on a post's content.

---
+++ backend/Capstone/posts/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+
+from core.utils import generate_random_string
+from posts.models import Post
+
+###############################
+# Function manaually assigns
+# the slug field to the new
+# instance before it's created.
+# Once saved, the new instance
+# can have it's own unique slug
+###############################
+@receiver(pre_save, sender=Post)
+def add_slug_to_post(sender, instance, *args, **kwargs):
+    if instance and not instance.slug:
+        slug = slugify(instance.content)
+        random_string = generate_random_string()
+        instance.slug = slug + "-" + random_string
