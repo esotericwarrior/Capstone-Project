cc2d3842bb2ce761ea77f82fd1df14dc25751d2a
Tristan <tristan.santiago@ymail.com>
2019-10-28 21:00:41 -0400

    Imported 'IsAuthenticated' permissions.

--- backend/Capstone/posts/api/views.py
+++ backend/Capstone/posts/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from posts.api.permissions import IsAuthorOrReadOnly
 from posts.api.serializers import CommentSerializer, PostSerializer
