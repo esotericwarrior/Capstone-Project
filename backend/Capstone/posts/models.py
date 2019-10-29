37fca98ea2a0bd473c09ae8afff0fcd64c2b7735
Tristan <tristan.santiago@ymail.com>
2019-10-28 20:23:32 -0400

    Defined Comment Class Model in '/posts/models.py'.

--- backend/Capstone/posts/models.py
+++ backend/Capstone/posts/models.py
@@ -12,3 +12,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class Comment(models.Model):    # Post Comment
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        body = models.TextField()
+        post = models.ForeignKey(Post,  # There can't be a comment without a post
+                                on_delete=models.CASCADE,
+                                related_name="comments")
+        author = models.ForeignKey(settings.AUTH_USER_MODEL,    # Each comment has an author
+                                    on_delete=models.CASCADE)
+        likers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name="likes")
+
+        def __str__(self):
+            return self.author.username
