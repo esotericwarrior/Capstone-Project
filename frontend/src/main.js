7bedf427da2525737cfec867cc1eeceea4497f1e
Tristan <tristan.santiago@ymail.com>
2019-10-29 19:32:59 -0400

    Added 'faUserCircle' Font Awesome Icon to library and imported in 'main.js'.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faUserCircle
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faUserCircle
+);
+
 
 Vue.config.productionTip = false;
 
