ac35cd1690dc3b255a31d74f34f46e636c978729
Tristan <tristan.santiago@ymail.com>
2019-10-28 20:51:45 -0400

    Defined Post View Set in 'api/views.py'.

---
+++ backend/Capstone/posts/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+
+from posts.api.serializers import CommentSerializer, PostSerializer
+from posts.models import Comment, Post
+
+class PostViewSet(viewsets.ModelViewSet):
+    """Provide CRUD +L functionality for Post."""
+    queryset = Post.objects.all()
+    lookup_field = "slug"
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
