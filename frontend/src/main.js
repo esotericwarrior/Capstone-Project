48b037bda4d573b17bc21278536696f199c7d23f
Tristan <tristan.santiago@ymail.com>
2019-10-29 19:50:35 -0400

    Added 'Bell' Font Awesome Icon to application library.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
+  faBell,
   faUserCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
+  faBell,
   faUserCircle
 );
 
