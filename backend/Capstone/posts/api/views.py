12a7195d330028a2efa468feaef4fe0206ce0ee7
Tristan <tristan.santiago@ymail.com>
2019-10-28 21:11:11 -0400

    Created Comment Create API View.

--- backend/Capstone/posts/api/views.py
+++ backend/Capstone/posts/api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from posts.api.permissions import IsAuthorOrReadOnly
@@ -14,3 +16,20 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class CommentCreateAPIView(generics.CreateAPIView):
+    """Allow users to Comment a Post instance if they haven't already."""
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        post = get_object_or_404(Post, slug=kwarg_slug)
+
+        # Raises a validation error if the user has already commented.
+        if post.comments.filter(author=request_user).exists():
+            raise ValidationError("You have already commented on this post!")
+
+        serializer.save(author=request_user, post=post)
