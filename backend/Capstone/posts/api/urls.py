e750261fa8bc1c462b560fc069b429a5bb759137
Tristan <tristan.santiago@ymail.com>
2019-10-28 21:12:55 -0400

    Updated 'posts/api/urls.py' to include path to comment on a specific post.

--- backend/Capstone/posts/api/urls.py
+++ backend/Capstone/posts/api/urls.py
@@ -8,4 +8,9 @@ router.register(r"posts", pv.PostViewSet) # Posts endpoint
 
 urlpatterns = [
     path("", include(router.urls)),
+
+    # Path to comment on a specific post.
+    path("posts/<slug:slug>/comment/",  # /posts/slug+randomly_generated_string/comment/
+         pv.CommentCreateAPIView.as_view(),
+         name="comment-create"),
 ]
