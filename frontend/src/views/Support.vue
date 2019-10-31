e53b8ad1d0e3756a5934e1ed179712465da4324b
Tristan <tristan.santiago@ymail.com>
2019-10-31 18:11:51 -0400

    Alphabetized data.

--- frontend/src/views/Support.vue
+++ frontend/src/views/Support.vue
@@ -8,16 +8,17 @@ export default {
     return {
       formSubmitted: false,
       email: "",
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ],
       message: "",
       name: "",
       nameRules: [
         v => !!v || "Name is required",
         v => (v && v.length <= 30) || "Name must be less than 30 characters"
       ],
-      emailRules: [
-        v => !!v || "E-mail is required",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-      ],
+      snackbar: false,
       submitResult: "",
       timeout: 3000,
       valid: true
