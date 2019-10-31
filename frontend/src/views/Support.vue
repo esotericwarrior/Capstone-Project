491003d45340553b6d382cef7d596afbcc0cfc75
Tristan <tristan.santiago@ymail.com>
2019-10-31 18:02:31 -0400

    Initialized 'formSubmitted' boolean.

--- frontend/src/views/Support.vue
+++ frontend/src/views/Support.vue
@@ -6,6 +6,7 @@ export default {
   name: "Support",
   data() {
     return {
+      formSubmitted: false,
       email: "",
       message: "",
       name: "",
@@ -20,7 +21,8 @@ export default {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "Invalid e-mail.";
         }
-      }
+      },
+      submitResult: ""
     };
   }
 };
