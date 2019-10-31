b3a79e519b886a8aaa8cc4439a7ab4f2d28cd598
Tristan <tristan.santiago@ymail.com>
2019-10-31 17:21:58 -0400

    Defined email 'rules' object.

--- frontend/src/views/Support.vue
+++ frontend/src/views/Support.vue
@@ -8,7 +8,15 @@ export default {
     return {
       email: "",
       message: "",
-      name: ""
+      name: "",
+      rules: {
+        required: value => !!value || "Required.",
+        counter: value => value.length <= 30 || "Max 30 characters",
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }
+      }
     };
   }
 };
