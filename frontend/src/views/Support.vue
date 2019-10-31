e39d962e9f97a85c0019880daa778d5987ee9a71
Tristan <tristan.santiago@ymail.com>
2019-10-31 15:57:32 -0400

    Initial commit.

---
+++ frontend/src/views/Support.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="support"></div>
+</template>
+<script>
+export default {
+  name: "Support",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped></style>
