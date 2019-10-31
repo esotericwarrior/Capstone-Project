2c50de809138bc0808caf4cbb2d50d915ac8379a
Tristan <tristan.santiago@ymail.com>
2019-10-31 14:37:17 -0400

    Added links to 'v-footer' component.

--- frontend/src/components/Application/Footer/views/Footer.vue
+++ frontend/src/components/Application/Footer/views/Footer.vue
@@ -1,6 +1,18 @@
 <template>
   <v-footer absolute color="primary lighten-1" padless>
-    <v-row justify="center" no-gutters> </v-row>
+    <v-row justify="center" no-gutters>
+      <!-- Footer Links -->
+      <v-btn
+        v-for="link in links"
+        :key="link.title"
+        color="white"
+        text
+        :to="link.path"
+        class="my-2"
+      >
+        {{ link.title }}
+      </v-btn>
+    </v-row>
   </v-footer>
 </template>
 <script>
