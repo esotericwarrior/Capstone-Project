2c8dbf96f52262b5b7896473adf04bf9588e0735
Tristan <tristan.santiago@ymail.com>
2019-11-01 13:51:51 -0400

    Created 'PostEditor' component.

---
+++ frontend/src/components/Application/Posts/views/PostEditor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+<script>
+
+export default {
+  name: "PostEditor",
+  data: {
+    return {
+
+    }
+  },
+  methods: {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
