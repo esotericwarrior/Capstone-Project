4aa43b8b210569ba19d8be8207f3291857235f68
Tristan <tristan.santiago@ymail.com>
2019-11-01 12:55:02 -0400

    Added tooltip to 'Home' button.

--- frontend/src/components/Application/Navigation/views/TopMenu.vue
+++ frontend/src/components/Application/Navigation/views/TopMenu.vue
@@ -3,9 +3,14 @@
     <!--Scrolling Toolbar -->
     <v-app-bar color="">
       <v-spacer></v-spacer>
-      <v-btn class="ml-4" icon to="/">
-        <font-awesome-icon icon="home" size="2x" />
-      </v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn class="ml-4" icon to="/">
+            <font-awesome-icon icon="home" size="2x" v-on="on" />
+          </v-btn>
+        </template>
+        <span>Home</span>
+      </v-tooltip>
       <v-divider class="mx-4" inset vertical></v-divider>
       <v-toolbar-title>
         <div class="pl-3">
