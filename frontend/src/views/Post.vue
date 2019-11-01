c8a4c7786bb32fa6e488b087c2b69e6902374708
Tristan <tristan.santiago@ymail.com>
2019-11-01 11:21:15 -0400

    Created 'Post.vue' to display individual posts.

---
+++ frontend/src/views/Post.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="single-post mt-2">
+    <div class="container">
+      {{ slug }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    slug: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
