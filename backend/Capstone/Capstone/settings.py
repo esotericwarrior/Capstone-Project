5acac8338b838a85074521d658c2f4fb7cb79239
Tristan <tristan.santiago@ymail.com>
2019-10-31 20:16:28 -0400

    Added list of origins that are authorized to make cross-site HTTP requests.

--- backend/Capstone/Capstone/settings.py
+++ backend/Capstone/Capstone/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = (
+    'https://localhost:8080',
+    'http://localhost:8080',
+    'https://localhost:8000'
+    'http://localhost:8000',
+    'https://127.0.0.1:8080',
+    'https://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:8000',
+    '127.0.0.1:8080'
+)
 
 # Application definition
 
