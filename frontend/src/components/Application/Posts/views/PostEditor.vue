f31c3783df5a5b6d745d447dca620f3a79808028
Tristan <tristan.santiago@ymail.com>
2019-11-01 14:03:33 -0400

    Defined 'onSubmit' method.

--- frontend/src/components/Application/Posts/views/PostEditor.vue
+++ frontend/src/components/Application/Posts/views/PostEditor.vue
@@ -13,7 +13,31 @@ export default {
       post_body: null
     };
   },
-  methods: {}
+  methods: {
+    onSubmit() {
+      // Tell the REST API to create or update a Post Instance
+      if (!this.post_body) {
+        this.error = "You can't send an empty post!";
+      } else if (this.post_body.length > 240) {
+        this.error = "Ensure this field has no more than 240 characters!";
+      } else {
+        let endpoint = "/api/posts/";
+        let method = "POST";
+        // if (this.slug !== undefined) {
+        //   endpoint += `${this.slug}/`;
+        //   method = "PUT";
+        // }
+        apiService(endpoint, method, { content: this.post_body }).then(
+          post_data => {
+            this.$router.push({
+              name: "post",
+              params: { slug: post_data.slug }
+            });
+          }
+        );
+      }
+    }
+  }
 };
 </script>
 
