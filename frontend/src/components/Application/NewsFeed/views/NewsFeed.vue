a43b9470dc73154647662dd6bcca7622c79640b5
Tristan <tristan.santiago@ymail.com>
2019-10-31 22:30:15 -0400

    Temporarily switched to fetch() requests.

--- frontend/src/components/Application/NewsFeed/views/NewsFeed.vue
+++ frontend/src/components/Application/NewsFeed/views/NewsFeed.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
+import { apiService } from "@/common/api.service";
 
 export default {
   name: "NewsFeed",
@@ -14,14 +15,22 @@ export default {
   },
   methods: {
     getPosts() {
-      axios
-        .get("http://127.0.0.1:8000/api/posts/")
-        .then(response => (this.posts = response.data.results))
-        // eslint-disable-next-line no-console
-        .catch(error => console.log(error));
+      // axios
+      //   .get("http://127.0.0.1:8000/api/posts/")
+      //   .then(response => (this.posts = response.data.results))
+      //   // eslint-disable-next-line no-console
+      //   .catch(error => console.log(error));
+      let endpoint = "api/posts/";
+      apiService(endpoint).then(data => {
+        this.posts.push(...data.results);
+      });
     }
   },
-  mounted() {}
+  mounted() {
+    this.getPosts();
+    // eslint-disable-next-line no-console
+    console.log(this.posts);
+  }
 };
 </script>
 <style lang="scss" scoped></style>
