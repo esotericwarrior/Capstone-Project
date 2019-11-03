dc347558130d745cf315db44b2401e0ef97496c5
Tristan <tristan.santiago@ymail.com>
2019-11-03 07:43:48 -0500

    Created 'CommentEditor.vue'.

---
+++ frontend/src/views/CommentEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import { apiService } from "@/common/api.service.js";
+export default {
+  name: "CommentsEditor",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
