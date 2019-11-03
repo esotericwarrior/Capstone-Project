a3cf9314760cb89eb75811953eb3e30761dde67d
Tristan <tristan.santiago@ymail.com>
2019-11-03 07:46:35 -0500

    Defined 'beforeRouteEnter' method.

--- frontend/src/views/CommentEditor.vue
+++ frontend/src/views/CommentEditor.vue
@@ -19,6 +19,14 @@ export default {
       postSlug: null
     };
   },
-  methods: {}
+  methods: {},
+  async beforeRouteEnter(to, from, next) {
+    // Get the comment's data from the REST API and set two data properties for the component
+    let endpoint = `/api/comments/${to.params.id}/`;
+    let data = await apiService(endpoint);
+    return next(
+      vm => ((vm.commentBody = data.body), (vm.postSlug = data.post_slug))
+    );
+  }
 };
 </script>
