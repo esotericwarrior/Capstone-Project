3b5be5dc690db69c0ae01889d9547934fcc84b5b
Tristan <tristan.santiago@ymail.com>
2019-11-17 13:11:20 -0500

    Added 'Cog' Font Awesome Icon to application library.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import vuetify from "./plugins/vuetify";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faBell,
+  faCog,
   faComment,
   faEdit,
   faHeart,
@@ -19,6 +20,7 @@ import {
 // Import the Icons used in the Application
 library.add(
   faBell,
+  faCog,
   faComment,
   faEdit,
   faHeart,
