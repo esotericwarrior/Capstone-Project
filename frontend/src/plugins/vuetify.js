e4cf103b4a6e1fea9e0bfc8548d1f181c051ec36
Tristan <tristan.santiago@ymail.com>
2019-11-17 17:49:52 -0500

    Migrated Font Awesome Icon import statements to 'vuetify.js'.

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,10 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBell,
+  faCog,
+  faComment,
+  faEdit,
+  faHeart,
+  faHome,
+  faPlusSquare,
+  faSearch,
+  faStar,
+  faTrash,
+  faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
+
+// Import the Icons used in the Application
+library.add(
+  faBell,
+  faCog,
+  faComment,
+  faEdit,
+  faHeart,
+  faHome,
+  faPlusSquare,
+  faSearch,
+  faStar,
+  faTrash,
+  faUserCircle
+);
+
+// Override Vuetify Icons with Custom FA Font Library
+const CUSTOM_ICONS = {
+  edit: "fas fa-edit",
+}
+
+Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi"
-  }
+    iconfont: 'fa',
+    values: CUSTOM_ICONS,
+  },
 });
