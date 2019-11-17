b9d07c450d3fc17dda2aa029fa00e7539e9f45a2
Tristan <tristan.santiago@ymail.com>
2019-11-17 12:00:21 -0500

    Added outer content wrapping.

--- frontend/src/components/Application/Profile/views/ProfileBanner.vue
+++ frontend/src/components/Application/Profile/views/ProfileBanner.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" offset-sm="3">
+        <v-row class="mb-6 mt-10" no-gutters> </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
