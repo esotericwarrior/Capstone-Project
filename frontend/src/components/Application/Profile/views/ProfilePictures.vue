e529b6ee11f272e30c7dc1f536ed86c955c972c9
Tristan <tristan.santiago@ymail.com>
2019-11-18 03:20:12 -0500

    Added placeholder content for 'Favorites'.

--- frontend/src/components/Application/Profile/views/ProfilePictures.vue
+++ frontend/src/components/Application/Profile/views/ProfilePictures.vue
@@ -57,6 +57,48 @@
               </v-container>
             </v-card>
           </v-tab-item>
+
+          <!-- Favorites -->
+          <v-tab-item>
+            <v-card color="grey lighten-5" flat tile>
+              <v-container fluid>
+                <v-row>
+                  <v-col
+                    v-for="n in 9"
+                    :key="n"
+                    class="d-flex child-flex"
+                    cols="4"
+                  >
+                    <v-card flat tile class="d-flex">
+                      <v-img
+                        :src="
+                          `https://picsum.photos/500/300?image=${n * 6 + 15}`
+                        "
+                        :lazy-src="
+                          `https://picsum.photos/10/6?image=${n * 6 + 15}`
+                        "
+                        aspect-ratio="1"
+                        class="grey lighten-2"
+                      >
+                        <template v-slot:placeholder>
+                          <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                          >
+                            <v-progress-circular
+                              indeterminate
+                              color="grey lighten-5"
+                            ></v-progress-circular>
+                          </v-row>
+                        </template>
+                      </v-img>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-tab-item>
         </v-tabs>
       </v-col>
     </v-row>
