b9a35d3a37e493ad90a3f75cccf6e22118468ba0
Tristan <tristan.santiago@ymail.com>
2019-11-18 03:17:31 -0500

    Added placeholder images for each post.

--- frontend/src/components/Application/Profile/views/ProfilePictures.vue
+++ frontend/src/components/Application/Profile/views/ProfilePictures.vue
@@ -22,7 +22,36 @@
                     class="d-flex child-flex"
                     cols="4"
                   >
-                    <v-card flat tile class="d-flex"> </v-card>
+                    <v-card flat tile class="d-flex">
+                      <v-img
+                        @click="
+                          openDialog(
+                            `https://picsum.photos/500/300?image=${n * 5 + 10}`
+                          )
+                        "
+                        :src="
+                          `https://picsum.photos/500/300?image=${n * 5 + 10}`
+                        "
+                        :lazy-src="
+                          `https://picsum.photos/10/6?image=${n * 5 + 10}`
+                        "
+                        aspect-ratio="1"
+                        class="grey lighten-2"
+                      >
+                        <template v-slot:placeholder>
+                          <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                          >
+                            <v-progress-circular
+                              indeterminate
+                              color="grey lighten-5"
+                            ></v-progress-circular>
+                          </v-row>
+                        </template>
+                      </v-img>
+                    </v-card>
                   </v-col>
                 </v-row>
               </v-container>
