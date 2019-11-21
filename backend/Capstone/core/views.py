a26b958e498051935666f26d6b7c64376c115939
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2019-11-21 14:30:15 -0500

    Update views.py
    Fixed incorrect logic.

--- backend/Capstone/core/views.py
+++ backend/Capstone/core/views.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 class IndexTemplateView(LoginRequiredMixin, TemplateView):
 
     def get_template_names(self):
-        if not settings.DEBUG:
+        if settings.DEBUG:
             template_name = "index-dev.html"
         else:
             template_name = "index.html"
