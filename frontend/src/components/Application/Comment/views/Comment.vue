32b3f27ea7bf8ba07e16942063ad6a4e49ff4afb
Tristan <tristan.santiago@ymail.com>
2019-11-30 21:33:02 -0500

    Added 'three-line' prop to 'v-list'.

--- frontend/src/components/Application/Comment/views/Comment.vue
+++ frontend/src/components/Application/Comment/views/Comment.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card max-width="800" class="mx-auto grey lighten-5" flat>
-    <v-list color="grey lighten-5">
+    <v-list color="grey lighten-5" three-line>
       <v-list-item>
         <v-tooltip bottom>
           <template v-slot:activator="{ on }">
