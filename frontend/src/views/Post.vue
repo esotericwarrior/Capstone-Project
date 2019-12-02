ac54f76dc218397c98d063809aa8cf7556faaf9c
Tristan <tristan.santiago@ymail.com>
2019-12-02 13:39:33 -0500

    Placed 'post.url' in 'v-img' component.

--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -4,7 +4,7 @@
       <v-col>
         <v-card class="mx-auto grey lighten-5" flat max-width="900">
           <div class="container">
-            <img :src="post.url" />
+            <v-img :src="post.url" height="600px" />
             <blockquote
               class="imgur-embed-pub"
               lang="en"
